20a88d8138fc5e4f4061428c44143d188d086cf1
Stephen Arment <sarment0704@gmail.com>
2023-05-02 14:13:15 -0700

    complete lab

---
+++ index.js
@@ -0,0 +1,16 @@
+function receivesAFunction(callback){
+    callback();
+}
+
+
+
+function returnsANamedFunction(){
+    return function namedfunction(){ };
+}
+
+
+function returnsAnAnonymousFunction(){
+    return function() {
+        
+    };
+}
